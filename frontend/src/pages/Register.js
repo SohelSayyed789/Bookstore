63bc831ff74bc95409b1ab527cf575bdca1dde94
sohel sayyed <sohelsayyed789@gmail.com>
2025-11-02 21:42:09 +0530

    Fix

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -25,7 +25,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/users/register", {
+      const response = await fetch("https://bookstore-zxrx.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
